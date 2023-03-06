dfc55ff80e7abf9447c32d7d6512aa8faeabc7e6
Sergio Reinoso <slrs993@gmail.com>
2023-03-05 20:29:20 -0500

    Frist course, Java primeros pasos

---
+++ java-primeros-pasos/src/EjemploCiclosFor.java
@@ -0,0 +1,11 @@
+
+public class EjemploCiclosFor {
+
+	public static void main(String[] args) {
+		//   variable         condiccion      ejecucion final
+		for(int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+		
+	}
+}
---
+++ java-primeros-pasos/src/TipoVariable.java
@@ -0,0 +1,21 @@
+
+public class TipoVariable {
+	
+	public static void main(String[] args) {
+		
+		double variable1 = 230.89;
+		int variable1Entero = (int) variable1;//Cast
+		//Este metodo imprime en consola
+		System.out.println(variable1Entero);
+		
+		long prueba = 1222222222222222222L;
+		short numeroPequeño = 13555;
+		byte numeroAunMasPequeño = 15;
+		float numeroDecimalPequeno = 2.5F;
+		
+		int resultado = (int) variable1 + variable1Entero;
+		System.out.println(resultado);
+		
+	}
+
+}
